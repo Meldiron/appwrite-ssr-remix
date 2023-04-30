131ab3c492070a2a6f49a86d5ce11be56ca1a50f
Matej Bačo <matejbaco2000@gmail.com>
2023-04-30 11:13:05 +0200

    add SSR

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,51 @@
 import { Fragment, useState } from "react";
-import { AppwriteService } from "~/AppwriteService";
+import { AppwriteProject, AppwriteService } from "~/AppwriteService";
 import Card from "~/components/card";
 import Layout from "~/components/layout";
 
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export function getCookie(cookieString: string, cookieName: string) {
+  const cookies = cookieString ? cookieString.split("; ") : [];
+  for (let i = 0; i < cookies.length; i++) {
+    const [name, value] = cookies[i].split("=");
+    if (name === cookieName) {
+      return decodeURIComponent(value);
+    }
+  }
+  return null;
+}
+
+// TODO: Ensure it runs ONLY ssr
+// TODO: Add screenshots to docs
+export const loader = async ({ request }: any) => {
+  const sessionNames = [
+    "a_session_" + AppwriteProject.toLowerCase(),
+    "a_session_" + AppwriteProject.toLowerCase() + "_legacy",
+  ];
+
+  const hash =
+    getCookie(request.headers.get("Cookie"), sessionNames[0]) ??
+    getCookie(request.headers.get("Cookie"), sessionNames[1]) ??
+    "";
+  AppwriteService.setSession(hash);
+
+  let account;
+  try {
+    account = await AppwriteService.getAccount();
+  } catch (err) {
+    account = null;
+  }
+
+  return json({
+    account,
+  });
+};
+
 export default function Index() {
+  const { account } = useLoaderData<typeof loader>();
+
   const [isLoading, setLoading] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
   const [modalType, setModalType] = useState("");
@@ -96,8 +138,12 @@ export default function Index() {
                 } as any
               }
             >
-              <li><Card isCsr={false} account={true} /></li>
-              <li><Card isCsr={true} /></li>
+              <li>
+                <Card isCsr={false} account={account} />
+              </li>
+              <li>
+                <Card isCsr={true} />
+              </li>
             </ul>
           </div>
           <div className="container">
